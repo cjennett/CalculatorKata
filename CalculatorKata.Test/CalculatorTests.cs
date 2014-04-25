748b372137da504ca034e4d3b145b6e8470da8b6
Chuck Jennett <cjennett@hotmail.com>
2014-04-25 12:50:12 -0400

    initial commit

---
+++ CalculatorKata.Test/CalculatorTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CalculatorKata.Test
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        Calculator calculator = new Calculator();
+
+        [Test]
+        public void Add_EmptyString_ReturnZero()
+        {
+            var result = calculator.Add("");
+            result.Should().Be(0);
+        }
+        [Test]
+        public void Add_NullString_ReturnZero()
+        {
+            var result = calculator.Add(null);
+            result.Should().Be(0);
+        }
+        [Test]
+        public void Add_SingleNumber_ReturnTheNumber()
+        {
+            var result = calculator.Add("1");
+            result.Should().Be(1);
+        }
+        [Test]
+        public void Add_TwoNumbers_ReturnTheSum()
+        {
+            var result = calculator.Add("1,2");
+            result.Should().Be(3);
+        }
+        [Test]
+        public void Add_TwoNumbersWithNewLine_ReturnTheSum()
+        {
+            var result = calculator.Add("1\n2");
+            result.Should().Be(3);
+        }
+        [Test]
+        public void Add_ThreeNumbersCombinedCommasAndNewLine_ReturnTheSum()
+        {
+            var result = calculator.Add("1\n2,3");
+            result.Should().Be(6);
+        }
+        [Test]
+        public void Add_CustomDelimiters_ReturnTheSum()
+        {
+            var result = calculator.Add("//;\n1;2");
+            result.Should().Be(3);
+        }
+        [Test]
+        public void Add_SingleNegativeNumber_ThrowsAnException()
+        {
+            Action act = () => calculator.Add("-1");
+
+            act.ShouldThrow<Exception>().WithMessage("-1");
+        }
+        [Test]
+        public void Add_SingleNegativeNumberSinglePositiveNumber_ThrowsAnException()
+        {
+            Action act = () => calculator.Add("-1,2");
+
+            act.ShouldThrow<Exception>().WithMessage("-1");
+        }
+        [Test]
+        public void Add_TwoNegativeNumber_ThrowsAnException()
+        {
+            Action act = () => calculator.Add("-1,-2");
+
+            act.ShouldThrow<Exception>().WithMessage("-1 -2");
+        }
+        [Test]
+        public void Add_BadParameter_ThrowsAnException()
+        {
+            Action act = () => calculator.Add("1-");
+
+            act.ShouldThrow<InvalidOperationException>().WithMessage("1-");
+        }
+
+    }
+}
---
+++ CalculatorKata/Calculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalculatorKata
+{
+    public class Calculator
+    {
+        public int Add(string values)
+        {
+            if (string.IsNullOrEmpty(values))
+                return 0;
+            return HasCustomerDelimiter(values) ? SumWithCustomerDelimiter(values) : SumWithDefaultDelimiters(values);
+        }
+
+        private static int SumWithDefaultDelimiters(string values)
+        {
+            var seperators = new[] {',', '\n'};
+            return Sum(values, seperators);
+        }
+
+        private static int SumWithCustomerDelimiter(string values)
+        {
+            var strings = values.Split('\n');
+            var seperators = Convert.ToChar(strings[0].Substring(2, 1));
+            return Sum(strings[1], seperators);
+        }
+
+        private static bool HasCustomerDelimiter(string values)
+        {
+            return values.StartsWith("//");
+        }
+
+        private static int Sum(string values, params char[] seperators)
+        {
+            var splitString = values.Split(seperators);
+            CheckForInvalidNumbers(splitString);
+            CheckForNegativeNumbers(splitString);
+
+            return splitString.Sum(value => Convert.ToInt32(value));
+
+        }
+
+        private static void CheckForNegativeNumbers(IEnumerable<string> splitString)
+        {
+            var negativeNumbers = string.Join(" ", splitString.Where(IsNegativeNumber).Select(s => s));
+            if(!string.IsNullOrEmpty(negativeNumbers))
+                throw new Exception(negativeNumbers.Trim());
+        }
+
+        private static void CheckForInvalidNumbers(IEnumerable<string> splitString)
+        {
+            var invalidNumbers = string.Join(" ", splitString.Where(s => !IsNumber(s)).Select(s => s));
+            if (!string.IsNullOrEmpty(invalidNumbers))
+                throw new InvalidOperationException(invalidNumbers);
+        }
+
+        private static bool IsNegativeNumber(string value)
+        {
+            return IsNumber(value) && Convert.ToInt32(value) < 0;
+        }
+
+        private static bool IsNumber(string value)
+        {
+            int num;
+            return int.TryParse(value, out num);
+        }
+    }
+}
